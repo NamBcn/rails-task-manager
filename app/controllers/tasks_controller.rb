9f80de674ce6390222093fdf9a90ff56d6a783e4
Nam Hai Chau <namhai.chau@gmail.com>
2019-05-14 17:12:15 +0200

    added destroy and create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,15 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new()
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
   def show
     @task = Task.find(params[:id])
   end
@@ -17,13 +26,18 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+     end
+
   private
     def task_params
       params.require(:task).permit(:title, :details, :completed)
     end
 end
-  # def completed
-  #   @completed = if true
+
 
 
 
