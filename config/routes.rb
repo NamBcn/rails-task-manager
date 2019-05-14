be8aa231ac5cf0617014de7ea1dd660c587235c6
Nam Hai Chau <namhai.chau@gmail.com>
2019-05-14 14:39:00 +0200

    added link to task list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index', as: :tasks
-  get '/tasks/:id', to: 'tasks#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/tasks/:id', to: 'tasks#show', as: :task  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
 # get '/ask', to: 'questions#ask', as: :ask
